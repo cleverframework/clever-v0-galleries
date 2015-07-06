3f553b288b1dd2b8b492b927da20a250ae351543
Jacopo Daeli <jacopo.daeli@gmail.com>
2015-07-06 17:33:08 +0100

    main.js file updated

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ let cleverCore = require('clever-core');
 let Package = cleverCore.Package;
 
 // Defining the Package
-var ImageGalleriesPackage = new Package('users-ssh');
+// var ImageGalleriesPackage = new Package('image-galleries');
 
 // All CLEVER packages require registration
-ImageGalleriesPackage.register(function(app, auth, database, storage) {
-
-  ImageGalleriesPackage.routes(app, auth, database, storage);
-
-  return ImageGalleriesPackage;
-
-});
+// ImageGalleriesPackage.register(function(app, auth, database, storage) {
+//
+//   ImageGalleriesPackage.routes(app, auth, database, storage);
+//
+//   return ImageGalleriesPackage;
+//
+// });
