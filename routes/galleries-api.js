12395d4e0d67b4601c1dba90145ef0dde421dfa8
Dev <jacopo.daeli@gmail.com>
2015-07-16 14:57:59 +0100

    navigation fixed

--- routes/galleries-api.js
+++ routes/galleries-api.js
@@ -36,36 +36,18 @@ module.exports = function(ImageGalleriesPackage, app, auth, database, storage) {
 
   // Delete gallery
   router.delete('/:id', auth.requiresAdmin, galleriesApiCtrl.deleteGallery);
-  //
+
   // // Get gallery images by galleryId
   // router.get('/:galleryId/images', galleriesApiCtrl.getGalleryImagesByGalleryId);
   //
   // // Get gallery images by gallerySlug
   // router.get('/:gallerySlug/images', galleriesApiCtrl.getGalleryImagesByGallerySlug);
-  //
+
   // Add images to gallery
   router.post('/:galleryId/images', auth.requiresAdmin, galleriesApiCtrl.addGalleryImages);
 
   // // Delete image to gallery by id
   // router.post('/:galleryId/images/:imageId', auth.requiresAdmin, galleriesApiCtrl.deleteGalleryImage));
-
-
-  /**
-   * Image
-   */
-
-  // // Get galleries (/images?tags[]=clever&tags[]=awesome)
-  // router.get('/images', galleriesApiCtrl.getImages);
-  //
-  // // Get gallery by id
-  // router.get('/images/:id', galleriesApiCtrl.getImageById);
-  //
-  // // Edit gallery image
-  // router.put('/images/:id', auth.requiresAdmin, galleriesApiCtrl.editGalleryImageById.bind(null, storage));
-  //
-  // // Delete gallery image
-  // router.delete('/images/:id', auth.requiresAdmin, galleriesApiCtrl.deleteGalleryImageById.bind(null, storage));
-
 
   return new CleverCore.CleverRoute(router, 'api', false);
 
